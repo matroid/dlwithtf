8ca7260cccb0a072421ea2ec3fb0b84b30cce430
Bharath Ramsundar <rbharath@stanford.edu>
2017-03-29 13:13:10 -0700

    Added some plots and stats

--- ch3/linear_regression_tf.py
+++ ch3/linear_regression_tf.py
@@ -2,10 +2,16 @@ import numpy as np
 np.random.seed(456)
 import  tensorflow as tf
 tf.set_random_seed(456)
+import matplotlib.pyplot as plt
+from scipy.stats import pearsonr
 
+def pearson_r2_score(y, y_pred):
+  """Computes Pearson R^2 (square of Pearson correlation)."""
+  return pearsonr(y, y_pred)[0]**2
+
+# Generate synthetic data
 d = 1
 N = 10
-
 w_true = 5
 b_true = 2
 noise_scale = .1
@@ -13,15 +19,25 @@ x_np = np.random.rand(N, d)
 noise = np.random.normal(scale=noise_scale, size=(N, d))
 y_np = np.reshape(w_true * x_np  + b_true + noise, (-1))
 
+# Save image of the data distribution
+plt.scatter(x_np, y_np)
+plt.xlabel("X")
+plt.ylabel("y")
+plt.title("Raw Linear Regression Data")
+plt.savefig("lr_data.png")
 
+
+# Generate tensorflow graph
 with tf.name_scope("placeholders"):
   x = tf.placeholder(tf.float32, (N, d))
   y = tf.placeholder(tf.float32, (N,))
 with tf.name_scope("weights"):
   W = tf.Variable(tf.random_normal((d, 1)))
   b = tf.Variable(tf.random_normal((1,)))
+with tf.name_scope("prediction"):
+  y_pred = tf.matmul(x, W) + b
 with tf.name_scope("loss"):
-  l = tf.reduce_sum((y - (tf.matmul(x, W) + b))**2)
+  l = tf.reduce_sum((y - y_pred)**2)
 with tf.name_scope("optim"):
   train_op = tf.train.AdamOptimizer(.01).minimize(l)
 
@@ -34,8 +50,25 @@ train_writer = tf.summary.FileWriter('/tmp/lr-train', tf.get_default_graph())
 n_steps = 1000
 with tf.Session() as sess:
   sess.run(tf.global_variables_initializer())
+  # Train model
   for i in range(n_steps):
     feed_dict = {x: x_np, y: y_np}
     _, summary, loss = sess.run([train_op, merged, l], feed_dict=feed_dict)
     print("loss: %f" % loss)
     train_writer.add_summary(summary, i)
+
+  # Make Predictions
+  y_pred_np = sess.run(y_pred, feed_dict={x: x_np})
+  
+
+y_pred_np = np.reshape(y_pred_np, -1)
+r2 = pearson_r2_score(y_np, y_pred_np)
+print("Pearson R^2: %f" % r2)
+
+# Clear figure
+plt.clf()
+plt.xlabel("Y-true")
+plt.ylabel("Y-pred")
+plt.title("Predicted versus true values")
+plt.scatter(y_np, y_pred_np)
+plt.savefig("lr_pred.png")
